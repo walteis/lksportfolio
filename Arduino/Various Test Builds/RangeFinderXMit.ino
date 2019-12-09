91316b86b6dc584dd50af63f54730f4af42018b7
walteis <walt.eis@gmail.com>
2019-12-09 09:27:00 -0500

    code cleanup

--- Arduino/Various Test Builds/RangeFinderXMit.ino
+++ Arduino/Various Test Builds/RangeFinderXMit.ino
@@ -1,29 +1,14 @@
 /*********************************************************************
-Program to display output from Ultrasonic range finder on a .96" OLED display.
+Program to send output from Ultrasonic range finder over 433MHz link
 
-As designed, uses .96" display and Make "Maker" board.
-
-Pinouts are:
-Display     Prototype board
--------     ---------------
- VCC        3.3v
- GND        GND
- SCL        A5 (SCL)
- SDA        A4 (SDA)
-
- Range Finder   Prototype board
+ Transmitter    Prototype board
  ------------   ---------------
  VCC            5v
  GND            GND
- TRIG           D6
- ECHO           D7
+ DA             D12
  
-+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-Based on example written by Limor Fried/Ladyada  for Adafruit Industries.  
-BSD license, check license.txt for more information
-All text above, and the splash screen must be included in any redistribution
-+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-*********************************************************************/
+**********************************************************************/
+
 #include <RH_ASK.h>
 #include <SPI.h>
 #include <Wire.h>
@@ -33,17 +18,17 @@ All text above, and the splash screen must be included in any redistribution
 #define TRIG_PIN 6
 #define ECHO_PIN 7
 
-
 // initialize range finder
 SR04 sr04 = SR04(ECHO_PIN,TRIG_PIN);
 long a;   // global to hold distance
+
 RH_ASK driver;
 
 /*
  * Setup screen defaults and constants
  *  
  */
-void setup()   {                  
+void setup() {                  
   Serial.begin(9600);
   if (!driver.init())
     Serial.println("init failed");
@@ -55,10 +40,8 @@ void setup()   {
 void loop() {
   
   long duration;
-
   
   // get rangefinder output
-  // a=sr04.Distance();
   duration = 0;
   digitalWrite(TRIG_PIN, LOW);
   delayMicroseconds(2);
@@ -68,7 +51,7 @@ void loop() {
   delayMicroseconds(2);
   duration = pulseIn(ECHO_PIN, HIGH, PULSE_TIMEOUT);
   a = (duration/2) / 73.914;  // result in inches
-  // distance = (duration/2) / 29.1;  // result in cm
+  // a = (duration/2) / 29.1;  // result in cm
 
   String sMessage = String(a);
   sMessage.concat("              ");
